0b4406dea036968e171663b9b5c36ab158019bb1
Eve Reichmann <ereichmann4@aol.com>
2020-05-26 14:27:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,10 @@
 def using_push(array, string)
   array.push(string)
 end 
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
 def colors_in_the_rainbow
   colors = ["red", "yellow"]
   new_color = ["blue" , "green"]
