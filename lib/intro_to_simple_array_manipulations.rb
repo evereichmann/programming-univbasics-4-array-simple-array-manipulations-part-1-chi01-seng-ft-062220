89d225f370acf365b925a929f9356eab069e8fbb
Eve Reichmann <ereichmann4@aol.com>
2020-05-26 14:23:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,11 +1,11 @@
 def colors_in_the_rainbow
-  colors = ["red"]
+  colors = ["red", "yellow"]
   new_color = "blue"
   colors.push(new_color)
 end
 
 def next_color
-  second_color = ["green"]
+  second_color = ["green", "orange"]
   another_color = "purple"
   second_color.push(another_color)
 end
